9a6c636e22a3b1c5c66055c1909f821c46133edc
Sapien <sidjha0001@gmail.com>
2023-02-19 21:26:24 -0700

    first

---
+++ lib/homepage.dart
@@ -0,0 +1,219 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  var _myPicColor = Get.theme.colorScheme.primary;
+
+  int _selectedWorkIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: _getLogo(scale: 0.8),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextButton(onPressed: () {}, child: const Text('About')),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child:
+                TextButton(onPressed: () {}, child: const Text('Experience')),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextButton(onPressed: () {}, child: const Text('Projects')),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextButton(onPressed: () {}, child: const Text('Contact')),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 0.15.sw),
+          child: SizedBox(
+            width: 1.sw,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 10.h),
+                Text('Hi, my name is',
+                    style: TextStyle(
+                        color: Get.theme.colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                        wordSpacing: 4.0)),
+                SizedBox(height: 10.h),
+                Text('Siddharth Jha.', style: Get.textTheme.displayLarge),
+                SizedBox(height: 10.h),
+                Text('I build things for the web and mobile.',
+                    style: Get.textTheme.displayMedium),
+                SizedBox(height: 10.h),
+                ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 500),
+                    child: const Text(
+                      'I\'m a software engineer based in Canada who specializes in building (and occasionally designing) exceptional digital experiences which are used by millions of people worldwide. Currently, I am working as a Reasearch Assistant at the University of Alberta.',
+                    )),
+                SizedBox(height: 50.h),
+                OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.android),
+                    label: const Text('Check out my most favourite project')),
+
+                //about me
+                SizedBox(height: 150.h),
+                Text('About Me',
+                    style: Get.textTheme.displayLarge!
+                        .copyWith(fontWeight: FontWeight.bold)),
+
+                SizedBox(height: 50.h),
+
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Hi, I'm Siddharth! Ever since I was in 10th grade, I've been tinkering with different programming languages and tools. One language that really stood out to me was Python, and I delved deep into it, reading multiple books like 'Head First Programming' to learn as much as I could.\n\nBut I didn't just stop at learning. I wanted to put my skills to the test and see what I could create. That's when I came up with the idea for a social watch together app, and I used Flutter and Firebase to develop it. To my surprise, it quickly gained popularity and grew to over 330,000 users!\n\nSince then, I've continued to develop my skills and create multiple mobile and web apps using Flutter. What drives me is the desire to solve complex problems and create innovative solutions that make a real difference in people's lives. I believe that my technical expertise, passion for programming, and willingness to learn make me a valuable asset to any development team.",
+                            ),
+                            SizedBox(height: 50.h),
+                            const Text(
+                                'Here are a few technologies I\'ve been working with recently:'),
+                            SizedBox(height: 30.h),
+                            Wrap(
+                              spacing: 4.0,
+                              runSpacing: 4.0,
+                              children: const [
+                                Chip(label: Text('Flutter')),
+                                Chip(label: Text('Dart')),
+                                Chip(label: Text('Firebase')),
+                                Chip(label: Text('Python')),
+                                Chip(label: Text('JavaScript')),
+                                Chip(label: Text('Node.js')),
+                                Chip(label: Text('Machine Learning')),
+                              ],
+                            )
+                          ]),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 32.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: MouseRegion(
+                                onEnter: (event) {
+                                  setState(() {
+                                    _myPicColor = Colors.transparent;
+                                  });
+                                },
+                                onExit: (event) {
+                                  setState(() {
+                                    _myPicColor = Get.theme.colorScheme.primary;
+                                  });
+                                },
+                                child: SizedBox(
+                                    height: 270.h,
+                                    child: Image.asset(
+                                      'images/my-photo.jpg',
+                                      fit: BoxFit.fill,
+                                      color: _myPicColor,
+                                      colorBlendMode: BlendMode.color,
+                                    )),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                //Work Experience
+                SizedBox(height: 150.h),
+                Text('Where I\'ve Worked',
+                    style: Get.textTheme.displayLarge!
+                        .copyWith(fontWeight: FontWeight.bold)),
+
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 300.h,
+                      child: NavigationRail(
+                          //backgroundColor: Colors.transparent,
+                          useIndicator: true,
+                          labelType: NavigationRailLabelType.selected,
+                          onDestinationSelected: (value) {
+                            setState(() {
+                              _selectedWorkIndex = value;
+                            });
+                          },
+                          destinations: const <NavigationRailDestination>[
+                            NavigationRailDestination(
+                              icon: Icon(Icons.favorite_border),
+                              selectedIcon: Icon(Icons.favorite),
+                              label: Text('U of A'),
+                            ),
+                            NavigationRailDestination(
+                              icon: Icon(Icons.bookmark_border),
+                              selectedIcon: Icon(Icons.book),
+                              label: Text('ImbueDesk'),
+                            ),
+                          ],
+                          selectedIndex: _selectedWorkIndex),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _getLogo({double? scale}) {
+    return Transform.scale(
+      scale: scale ?? 1,
+      child: SizedBox(
+        height: 100,
+        child: Stack(
+          children: [
+            Image.asset(
+              'images/logo-bg.png',
+              color: Get.theme.colorScheme.primary,
+              height: 100,
+            ),
+            Positioned(
+              bottom: 25.0,
+              left: 38.0,
+              child: AutoSizeText('SJ',
+                  style: TextStyle(
+                      color: Get.theme.colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                      wordSpacing: 4.0)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/homepage.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,16 +14,38 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World!'),
-        ),
-        body: const Center(
-          child: Text(
-              'Hello World! Relax I am building something cool!.. Give me some time!'),
-        ),
-      ),
-    );
+    return ScreenUtilInit(
+        minTextAdapt: true,
+        builder: (c, w) {
+          return GetMaterialApp(
+            theme: FlexThemeData.light(
+              scheme: FlexScheme.bahamaBlue,
+              surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+              blendLevel: 9,
+              subThemesData: const FlexSubThemesData(
+                blendOnLevel: 10,
+                blendOnColors: false,
+              ),
+              visualDensity: FlexColorScheme.comfortablePlatformDensity,
+              useMaterial3: true,
+              swapLegacyOnMaterial3: true,
+              fontFamily: GoogleFonts.poppins().fontFamily,
+            ),
+            darkTheme: FlexThemeData.dark(
+              scheme: FlexScheme.bahamaBlue,
+              surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+              blendLevel: 15,
+              subThemesData: const FlexSubThemesData(
+                blendOnLevel: 20,
+              ),
+              visualDensity: FlexColorScheme.comfortablePlatformDensity,
+              useMaterial3: true,
+              swapLegacyOnMaterial3: true,
+              fontFamily: GoogleFonts.poppins().fontFamily,
+            ),
+            themeMode: ThemeMode.dark,
+            home: const Homepage(),
+          );
+        });
   }
 }
